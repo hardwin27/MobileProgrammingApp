b08a3cb0c3b4d3c32d55d429067972250c427db7
Christiannw <christiancnw.cn@gmail.com>
2021-01-31 18:16:08 +0700

    SplashScreen

--- app/src/main/java/com/example/mobileprogrammingapp/MainActivity.java
+++ app/src/main/java/com/example/mobileprogrammingapp/MainActivity.java
@@ -2,13 +2,48 @@ package com.example.mobileprogrammingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN = 3000; //duration to move to other page
+
+    //variables
+    Animation topAnim, bottomAnim;
+    ImageView image;
+    TextView title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+
+        //Animation
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+        //hooks
+        image = findViewById(R.id.imageView);
+        title = findViewById(R.id.textView3);
+
+        image.setAnimation(topAnim);
+        title.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() { //move to other activity
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, Dashboard.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN);
     }
 }
